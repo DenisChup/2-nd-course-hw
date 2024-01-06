13976d856a15bb97e9be278c82d933a197cdce35
DenisChup <zhykd@mail.ru>
2024-01-06 21:05:35 +0300

    Дз 2.2 9.0

---
+++ static/style/index.js
@@ -0,0 +1,32 @@
+alert('20');
+
+let iphone = 2007;
+alert(iphone);
+
+const javascript = 'BrendanEich';
+alert(javascript);
+
+let first = 10;
+let second = 2;
+alert(first+second);
+alert(first-second);
+alert(first*second);
+alert(first/second);
+let result = (second**5);
+alert(result);
+
+let a = 9;
+let b = 2;
+alert(a/b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num +=1;
+num -= 1;
+alert(num);
+
+let age = prompt('Сколько вам лет?');
+alert(age);
