8482c613581853fe99d35673558e5a9b972e427f
DenisChup <zhykd@mail.ru>
2024-01-26 14:30:25 +0300

    ДЗ 2.4

--- JS/HW 2.4/index.js
+++ JS/HW 2.4/index.js
@@ -1,1 +1,46 @@
 // задание 1
+for (let a = 0; a < 2; a++) {
+  console.log("Привет");
+}
+
+// задание 2
+
+for (let a = 1; a <= 5; a++) {
+  console.log("a");
+}
+
+// задание 3
+
+for (let a = 7; a <= 22; a++) {
+  console.log("a");
+}
+
+// задание 4
+
+let obj = {
+  Коля: "200",
+  Вася: "300",
+  Петя: "400",
+};
+for (let key in obj) {
+  console.log(`${key} - зарплата ${obj[key]} долларов.`);
+}
+
+// задание 5
+
+let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+  n /= 2;
+  num++;
+}
+console.log(n)
+console.log(num)
+
+// задание 6
+
+let firstFriday = 2;
+for (let a = firstFriday; a <= 31; a += 7) {
+  console.log(`Сегодня пятница, ${a}-е число. Необходимо подготовить отчет.`);
+}
